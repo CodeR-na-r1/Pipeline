d1579090b14e3ae50bbcaca0ccda54a65f1329f0
Vlad Sizyh <sizyh1375@gmail.com>
2024-03-03 19:05:49 +0800

    Create data objects for launch stages in multi-threading mode [no-stable]

--- sourceGenerator.cpp
+++ sourceGenerator.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
+#include <memory>
+
 #include <vector>
+#include <unordered_map>
 
 #include "zmq.hpp"
 #include "zmq_addon.hpp"
@@ -20,11 +23,42 @@
 #include "boost/numeric/ublas/tensor/tensor.hpp"
 #include <boost/numeric/ublas/tensor/extents.hpp>   // extents == shape
 
+#include "parser/parser.hpp"
 #include "pipeline/stage.hpp"
 
 using namespace std;
 using TensorT = boost::numeric::ublas::tensor<double>;
 using ShapeT = boost::numeric::ublas::shape;
+
+Pipeline::Stage<TensorT> getStages() {
+
+    ifstream fileJsonConfig("../pipeConfig.json");
+
+    if (!fileJsonConfig.is_open()) {
+        cerr << "File from object ifstream \'fileJsonConfig\' not open!" << endl;
+        throw std::exception{ "FILE CONFIG NOT FOUND" };
+    }
+
+    Pipeline::Stage<TensorT> source = Pipeline::JsonParser::fromFile<TensorT>(fileJsonConfig, [](std::string callableName) -> function<TensorT(TensorT)> {
+
+        if (callableName == "getImage") {
+            return [](TensorT data) { return data; };
+        }
+        else if (callableName == "rgb2gray") {
+            return [](TensorT data) { return data; };
+        }
+        else if (callableName == "gaussian") {
+            return [](TensorT data) { return data; };
+        }
+        else if (callableName == "display") {
+            return [](TensorT data) { cout << data[0]; return data; };
+        }
+
+        return [](TensorT data) { return data; };
+        }).value_or(Pipeline::Stage<TensorT>{ [](TensorT data) {return data; }, {}, {} });
+
+        return source;
+}
 
 int main() {
 
@@ -38,7 +72,7 @@ int main() {
 
         zmq::context_t ctx;
 
-        boost::lockfree::spsc_queue<TensorT, boost::lockfree::capacity<1024> > rawDataQ;  // buffer
+        boost::lockfree::spsc_queue<std::shared_ptr<TensorT>, boost::lockfree::capacity<1024> > rawDataQ;  // buffer
 
         //boost::asio::io_service ioService;  // for threads (::run)
         boost::thread_group networkThreadpool;
@@ -91,15 +125,16 @@ int main() {
 
                     //
                     // data
-                    TensorT tensor{ shapeTensor };
+                    TensorT* tensor = new TensorT{ shapeTensor };
 
                     auto data = ndarray.getData().begin();
-                    for (size_t i(0ul); i < tensor.extents().product(); ++i)
+                    for (size_t i(0ul); i < tensor->extents().product(); ++i)
                     {
-                        tensor[i] = *(data++);  // I have not been able to use std::copy here :(
+                        (*tensor)[i] = *(data++);  // I have not been able to use std::copy here :(
                     }
                     
-                    while (!rawDataQ.push(tensor)) {}
+                    std::shared_ptr<TensorT> temp{ tensor };
+                    while (!rawDataQ.push(temp)) {}
 
                     msg.rebuild();  // CHECK THIS
                 }
@@ -109,7 +144,7 @@ int main() {
         cout << "size queue -> " << rawDataQ.read_available() << endl;
 
         cout << "Wait while input queue will filled" << endl;
-        Sleep(10000);    // work emulation
+        //Sleep(10000);    // work emulation
 
         cout << "size queue -> " << rawDataQ.read_available() << endl;
 
@@ -118,7 +153,41 @@ int main() {
         // Pipeline -> 'ProcessingManager' {begin}
 
         // Creating queue for each stage
-        std::unordered_map<size_t, std::shared_ptr<boost::lockfree::spsc_queue<boost::numeric::ublas::tensor<double>>>> qMap;
+        Pipeline::Stage<TensorT> stages = getStages();
+        cout << stages << endl;
+        cout << "ID -> " << stages.childs[1].id << endl;
+
+        // create map for stages launch in multi-threading
+
+        std::unordered_map<size_t, Pipeline::Stage<TensorT>> stagesMap; // id ->  stage by id
+        std::unordered_map<size_t, std::shared_ptr<boost::lockfree::spsc_queue<std::shared_ptr<TensorT>>>> inputQueueMap; // get parent queue for current stage by id (i.e. get input queue)
+        std::unordered_map<size_t, std::vector<std::shared_ptr<boost::lockfree::spsc_queue<std::shared_ptr<TensorT>>>>> outputQueueMap; // get parent queue for current stage by id (i.e. get input queue)
+
+        // fill 1 map
+        std::queue<Pipeline::Stage<TensorT>*> queue;
+        queue.push(&stages);
+
+        while (!queue.empty()) {
+
+            auto current = queue.front();
+
+            outputQueueMap.insert({ current->id, {} });
+            outputQueueMap.at(current->id).reserve(current->childs.size());
+
+            for (auto&& child: current->childs) {
+
+                std::shared_ptr<boost::lockfree::spsc_queue<std::shared_ptr<TensorT>>> temp = { {} };
+                outputQueueMap.at(current->id).push_back(temp);
+                inputQueueMap.insert({ child.id , temp });
+                queue.push(&child);
+            }
+
+            stagesMap.insert({ current->id, (*current) });
+
+            queue.pop();
+        }
+
+        // fill 2 and 3 map's
 
 
         
