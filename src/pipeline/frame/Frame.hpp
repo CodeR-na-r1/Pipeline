57c7b8fc389eef343e9c1f183ae92a35f448c3aa
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-05 21:48:38 +0800

    imlement Frame structure [--no stable]

---
+++ src/pipeline/frame/Frame.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "IFrame.hpp"
+
+namespace Pipeline {
+
+	namespace Frame {
+
+		template <typename DataT>
+		class Frame : public IFrame<DataT> {
+
+			DataT data;
+
+		public:
+
+			Frame(const DataT& data) : IFrame<DataT>(), data(data) {}
+
+			virtual DataT getData() override {
+				return data;
+			}
+		};
+	}
+}
