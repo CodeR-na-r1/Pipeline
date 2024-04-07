7d3eb7f65ed4aacadd97475bc47892fbe1c41fac
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-07 15:24:54 +0800

    adaptate test for stage [--no stable]

--- test/src/pipeline/stageTest.cpp
+++ test/src/pipeline/stageTest.cpp
@@ -4,54 +4,67 @@
 
 #include <sstream>
 
-#include "pipeline/stage.hpp"
+#include "pipeline/Stage.hpp"
 
 BOOST_AUTO_TEST_SUITE(StageTest)
 
 BOOST_AUTO_TEST_CASE(TestAddChildForStage) {
 
-	using Pipeline::Stage;
+	using Pipeline::Stage::Stage;
 
-	Stage source = Stage{ std::function([](int data) { return data; }), "source", {} };
+	Stage source = { std::function([](int data) { return data; }), "source", {} };
+	BOOST_CHECK(source.getId() == 0);
+	BOOST_CHECK(source.getChilds().size() == 0);
 
-	BOOST_CHECK(source.childs.size() == 0);
+	Stage child = { std::function([](int data) { return data; }), "child", {} };
+	BOOST_CHECK(child.getId() == 1);
+	BOOST_CHECK(child.getChilds().size() == 0);
 
-	Stage child = Stage{ std::function([](int data) { return data; }), "child", {} };
-	source.childs.push_back(child);
+	source.addChild(child);
+	BOOST_CHECK(source.getChilds().size() == 1);
+	BOOST_CHECK(source.getId() == 0);
+	BOOST_CHECK(child.getId() == 1);
+	BOOST_CHECK(source.getChilds()[0].getId() == 2);
 
-	BOOST_CHECK(source.childs.size() == 1);
-	BOOST_CHECK(source.childs[0].name == "child");
+	source.addChild(std::move(child));
+	BOOST_CHECK(source.getChilds().size() == 2);
+	BOOST_CHECK(source.getId() == 0);
+	BOOST_CHECK(source.getChilds()[0].getId() == 2);
+	BOOST_CHECK(source.getChilds()[1].getId() == 1);
+
+	BOOST_CHECK(source.getChilds()[0].getName() == "child");
+	BOOST_CHECK(source.getChilds()[1].getName() == "child");
 }
 
 BOOST_AUTO_TEST_CASE(TestCallableStage) {
 
-	using Pipeline::Stage;
+	using Pipeline::Stage::Stage;
 
 	Stage source = Stage{ std::function([](int data) { return data * 2; }), "source", {} };
 
 	int data{ 3 };
-	BOOST_CHECK(source.callable(data) == data * 2);
+	BOOST_CHECK(source.getCallable()(data) == data * 2);
 }
 
 BOOST_AUTO_TEST_CASE(TestCallableStageForChilds) {
 
-	using Pipeline::Stage;
+	using Pipeline::Stage::Stage;
 
 	Stage source = Stage{ std::function([](int data) { return data * 2; }), "source", {} };
 	Stage child = Stage{ std::function([](int data) { return data * 3; }), "child", {} };
-	source.childs.push_back(child);
+	source.getChilds().push_back(child);
 
 	int data{ 3 };
 
-	BOOST_CHECK(source.callable(data) == data * 2);
+	BOOST_CHECK(source.getCallable()(data) == data * 2);
 
-	BOOST_CHECK(child.callable(data) == data * 3);
-	BOOST_CHECK(source.childs[0].callable(data) == data * 3);
+	BOOST_CHECK(child.getCallable()(data) == data * 3);
+	BOOST_CHECK(source.getChilds()[0].getCallable()(data) == data * 3);
 }
 
 BOOST_AUTO_TEST_CASE(TestCreationStagesOfPipeline) {
 
-	using Pipeline::Stage;
+	using Pipeline::Stage::Stage;
 
 	Stage source = Stage{ std::function([](int data) { return data; }), "SOURCE", {} };
 	Stage blur = { std::function([](int data) { return data; }), "BLUR", {} };
@@ -60,15 +73,14 @@ BOOST_AUTO_TEST_CASE(TestCreationStagesOfPipeline) {
 	Stage display = { std::function([](int data) { return data; }), "DISPLAY", {} };
 
 	// in reverse order
-
-	convert.addChild(binarize);
+	convert.addChild(std::move(binarize));
 	convert.addChild(display);
 
-	blur.addChild(convert);
+	blur.addChild(std::move(convert));
 	blur.addChild(display);
 
-	source.addChild(blur);
-	source.addChild(display);
+	source.addChild(std::move(blur));
+	source.addChild(std::move(display));
 
 	std::ostringstream ss{};
 	source.dump(ss);
