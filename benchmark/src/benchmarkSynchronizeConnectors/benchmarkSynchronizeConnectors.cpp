89db2bf841b37fe8ecf23b1d55c2b1434255c2f1
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-08 23:56:33 +0800

    write benchmark for syncConnector [--no stable]

---
+++ benchmark/src/benchmarkSynchronizeConnectors/benchmarkSynchronizeConnectors.cpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "connector/SyncMPMCConnector.hpp"
+
+#include <iostream>
+#include <thread>
+
+#define QUANTITY 10'000'000
+
+using Pipeline::Connector::SyncMapConnector;
+using Pipeline::Frame::IDFrame;
+
+using idT = std::uint64_t;
+using DataT = std::uint64_t;
+using FrameT = std::shared_ptr<IDFrame<idT, DataT>>;
+using SyncMapConnectorT = std::shared_ptr<SyncMapConnector<FrameT, 1024>>;
+
+using namespace std;
+
+int main() {
+
+	std::cout << "Parameters for benchmark: " << std::endl;
+	std::cout << "Amount of elements: " << QUANTITY << std::endl;
+	std::cout << "Size of connector (megabytes): " << (QUANTITY * sizeof(FrameT)) / 1024 / 1024 << std::endl;
+
+	// test syncMapConnector
+	{
+		// create connector
+		auto syncConnector = SyncMapConnectorT{ new Pipeline::Connector::SyncMapConnector<FrameT, 1024>{} };
+
+		// create timer points
+		std::chrono::steady_clock::time_point startWorkTime{}, finishWorkTime{};
+
+		std::cout << "Benchmark for test map sync connector started!" << std::endl;
+
+		// consumer thread
+		std::thread consumerSyncMapConnector{ [syncConnector, &finishWorkTime]() {
+
+			uint64_t counter{}, accumulator{};
+			FrameT value;
+
+			while (true) {
+
+				if (syncConnector->hasValue()) {
+
+					syncConnector->extract(value);
+					accumulator += value->getData();
+					++counter;
+				}
+				if (counter == QUANTITY) {
+					break;
+				}
+			}
+			finishWorkTime = std::chrono::high_resolution_clock::now();
+			std::cout << "Consumer thread with map sync connector finished!\tAccumulator: " << accumulator << ";\tCounter: " << counter << std::endl;
+			} };
+
+		// producer thread
+		std::thread producerSyncMapConnector{ [syncConnector, &startWorkTime]() {
+
+			startWorkTime = std::chrono::high_resolution_clock::now();
+
+			for (size_t i{ 0 }; i < QUANTITY; ++i)
+				while (!syncConnector->push(FrameT{ new IDFrame<idT, DataT>{i, i} })) {}
+			} };
+
+		// join threads
+		producerSyncMapConnector.join();
+		consumerSyncMapConnector.join();
+
+		auto timer = std::chrono::duration<double, std::milli>(finishWorkTime - startWorkTime).count();
+
+		// print result
+		std::cout << "Time for syncMapConnector -> " << timer << "ms" << std::endl;
+	}
+
+	return 0;
+}
