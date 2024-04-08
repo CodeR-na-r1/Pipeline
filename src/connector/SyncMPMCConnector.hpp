591d3e34d0a8cd4843e5eba2c21342095e6567b8
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-08 18:44:47 +0800

    write syncMPMC connector [--no stable]

---
+++ src/connector/SyncMPMCConnector.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "IConnector.hpp"
+
+#include "../detail/connector/SyncMapConnector.hpp"
+
+#include "pipeline/frame/IDFrame.hpp"
+
+namespace Pipeline {
+
+	namespace Connector {
+
+		template <typename DataT, size_t queueCapacity>
+		class SyncMapConnector : public IConnector<DataT> { };
+
+
+		template <typename IDType, typename DataT, size_t queueCapacity>
+		class SyncMapConnector<std::shared_ptr<Frame::IDFrame<IDType, DataT>>, queueCapacity> : public IConnector<std::shared_ptr<Frame::IDFrame<IDType, DataT>>> {
+
+			using FrameDataT = std::shared_ptr<Frame::IDFrame<IDType, DataT>>;
+
+			using containerT = detail::SyncMapConnector<IDType, FrameDataT>;
+
+			containerT storage;
+
+		public:
+
+			SyncMapConnector() : IConnector<FrameDataT>(), storage(queueCapacity) {}
+
+			virtual bool push(const FrameDataT& data) override {
+				return storage.insert(std::pair{ data->getID(), data }).second;
+			}
+
+			virtual bool hasValue() override {
+				return storage.hasValue();
+			}
+
+			virtual std::size_t size() override {
+				return storage.size();
+			}
+
+			virtual bool extract(FrameDataT& data) override {
+				return storage.extract(data);
+			}
+		};
+	}
+}
