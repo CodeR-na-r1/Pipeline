774a6bd4de44ae23dc5a56224749c8ca41df4cd9
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-09 13:18:20 +0800

    stageBuilder ready [--no stable]

---
+++ src/builder/StageBuilder.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <concepts>
+#include <functional>
+#include <stdexcept>
+
+#include "../builder/IStageBuilder.hpp"
+#include "../detail/chooser/Chooser.hpp"
+#include "../Parser/Parser.hpp"
+#include "../StageManager/StageManager.hpp"
+
+#include "StageAssembly.hpp"
+
+namespace Pipeline {
+
+	namespace Builder {
+
+		template <typename Traits>	// use decltype from user code and add concept in this place
+		class StageBuilder : public IStageBuilder<typename Traits::DataT, typename Traits::DaoT> {
+
+			using DataT = typename Traits::DataT;	//	typename maybe
+			using BrokerT = typename Traits::BrokerT;
+			using DaoT = typename Traits::DaoT;
+
+			std::string configFilename{};
+			detail::Chooser<DataT> _chooser{};
+
+			Stage::Stage<DataT> stages{};
+
+		public:
+
+			StageBuilder() : IStageBuilder<DataT, DaoT>() {}
+
+			StageBuilder& addConfigFile(const std::string& configFilename) {
+
+				this->configFilename = configFilename;
+
+				return *this;
+			}
+
+			StageBuilder& addChooser(const std::pair<std::string, std::function<DataT(DataT)>>& chooser) {
+
+				_chooser.addChoser(chooser);
+
+				return *this;
+			}
+
+			template <typename ItType>	// ADD CONCEPT <std::input_iterator ItF, std::input_iterator ItL>
+			StageBuilder& addChoosers(ItType first, ItType last) {
+
+				while (first != last) {
+
+					_chooser.addChoser(*first);
+					++first;
+				}
+
+				return *this;
+			}
+
+			// CHECK THIS METHOD -> return dangerous!
+			[[nodiscard]]
+			std::unique_ptr<IStageBuilder<DataT, DaoT>> build() {
+
+				// parse config file
+				std::ifstream configFile{ configFilename };
+
+				if (!configFile.is_open())
+					throw std::runtime_error{ "Config file not open! <from Stagebuilder>" };
+
+				auto&& resParser = Parser::JsonParser::fromFile<DataT>(configFile, std::shared_ptr<detail::IChooser<DataT>>(new detail::Chooser<DataT>{ std::move(_chooser) }));
+
+				if (!resParser.has_value())
+					throw std::runtime_error{ "Configuration error! <from parser>" };
+
+				stages = std::move(resParser.value());
+
+				configFile.close();
+
+				return std::unique_ptr<IStageBuilder<DataT, DaoT>>{ new StageBuilder<Traits>{ std::move(*this) }};
+			}
+
+			[[nodiscard]]
+			virtual std::shared_ptr<Stage::IStageManager<DataT, DaoT>> build(std::function<DataT(DaoT)> mapperFromDao, std::function<DaoT(DaoT, DataT)> mapperToDao) override {
+
+				StageAssembly<DataT, DaoT> sAsm{};
+
+				sAsm.initStages(std::move(stages));
+
+				sAsm.initCallables(mapperFromDao, mapperToDao);
+
+				return std::shared_ptr<Stage::IStageManager<DataT, DaoT>>{ new Stage::StageManager<DataT, DaoT>{ std::move(sAsm.stagesMap), sAsm.inputQueueMap, sAsm.outputQueuesMap, sAsm.measurementsMap, sAsm.stageCallables }};
+			}
+		};
+	}
+}
