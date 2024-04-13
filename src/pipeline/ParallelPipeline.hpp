a22d367eb4ea6bd4a2853bd791870ae4d28d9351
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-14 01:16:32 +0800

    create parallel type of pipeline [--no stable]

---
+++ src/pipeline/ParallelPipeline.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "../detail/pipeline/ParallelPipelineComponents.hpp"
+
+#include "boost/thread/thread.hpp"
+
+namespace Pipeline {
+
+	template <typename DataT, typename DaoT>
+	class ParallelPipeline {
+
+		detail::ParallelPipelineComponents<DataT, DaoT> components{};
+
+		boost::thread_group rNetworkMTPool;
+		boost::thread_group stagesMTPool;
+		boost::thread_group monitoringMTPool;
+
+	public:
+
+		ParallelPipeline(detail::ParallelPipelineComponents<DataT, DaoT> components) : components(components) {}
+
+		void start() {
+
+			for (auto&& it : components.rNetworkM) {
+
+				rNetworkMTPool.create_thread([rNM = it]() {
+
+					while (true) {
+
+						boost::this_thread::interruption_point();
+
+						rNM->operator()();
+					}
+					});
+			}
+
+			for (auto&& it : components.stagesM->getStageCallables()) {
+
+				stagesMTPool.create_thread([callable = it.second]() {
+
+					while (true) {
+
+						boost::this_thread::interruption_point();
+
+						callable();
+					}
+					});
+			}
+
+			for (auto&& it : components.monitoringM->getCallables()) {
+
+				monitoringMTPool.create_thread([it]() {
+
+					while (true) {
+
+						boost::this_thread::interruption_point();
+
+						it();
+					}
+				});
+			}
+		}
+
+		void stop() {
+
+			rNetworkMTPool.interrupt_all();
+			stagesMTPool.interrupt_all();
+			monitoringMTPool.interrupt_all();
+
+			rNetworkMTPool.join_all();
+			stagesMTPool.join_all();
+			monitoringMTPool.join_all();
+		}
+	};
+}
