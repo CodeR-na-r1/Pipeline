689fe25b186a02ade4c7ed35a6a306a328577891
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-14 01:17:04 +0800

    inputDistributor first prototype ready [--no stable]

--- src/utils/inputDistributor.hpp
+++ src/utils/inputDistributor.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <stdexcept>
 
+#include <string>
 #include <vector>
 
 #include "../builder/ReceiverNetworkManagerBuilder.hpp"
@@ -15,13 +16,18 @@
 namespace Pipeline {
 
 	namespace Utils {
-
+			
 		using nlohmann::json;
 
-		template <typename pipeBuilderT, typename Traits>
-		pipeBuilderT inputDistributor(pipeBuilderT&& pipeBuilder, std::ifstream& configFile) {
+		template <typename Traits>
+		decltype(auto) inputDistributor(auto& pipeBuilder, const std::string& configFileName) {
 
+			std::ifstream configFile{ configFileName };
+			if (!configFile.is_open()) {
+				throw std::runtime_error{ "File with configuration not found! <from function \'inputDistributor\'>" };
+			}
 			json data = json::parse(configFile);
+			configFile.close();
 
 			if (data.contains("in")) {
 
@@ -31,12 +37,15 @@ namespace Pipeline {
 
 					if (input.contains("ip") && input.contains("port")) {
 
+						auto&& ip = input["ip"].get<std::string>();
+						auto&& port = std::atoi(input["port"].get<std::string>().c_str());
+
 						pipeBuilder.addReceiverNetworkManager(
 							Builder::ReceiverNetworkManagerBuilder<Traits>{}
-							.setBroker(Broker::ZmqReceiverBroker{ input["ip"], input["port"] }.build())
-							.setDeserializator(Proto::NDArrayDeserializator<typename Traits::DataT>{}.build())
+								.setBroker(Broker::ZmqReceiverBroker{ ip, port }.build())
+								.setDeserializator(Proto::NDArrayDeserializator<typename Traits::DataT>{}.build())
 							.build()
-						);
+							);
 					}
 					else {
 						throw std::runtime_error{ "Error configuration parse in \'inputDistributor\'. Ip or port not set for input!" };
@@ -44,7 +53,7 @@ namespace Pipeline {
 				}
 			}
 
-			return std::forward(pipeBuilder);
+			return pipeBuilder;
 		}
 	}
 }
