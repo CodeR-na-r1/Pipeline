822f04c729a642424d59bbf08f44d51c1883efa9
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-17 22:44:20 +0800

    write Scalableproperty parser [--no stable]

---
+++ src/parser/ScalablePropertyParser.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "json/json.hpp"
+
+#include "../pipeline/Stage.hpp"
+#include "../detail/functions/StageFunctions.hpp"
+
+#include <fstream>
+
+#include <unordered_set>
+#include <optional>
+
+namespace Pipeline {
+
+	namespace Parser {
+
+		using nlohmann::json;
+
+		struct ScalablePropertiesParser {
+
+			template <typename TData>
+			static std::optional<std::unordered_set<std::size_t>> getScalableProperties(std::ifstream& f, const Stage::Stage<TData>& stages) {
+
+				json data = json::parse(f);
+
+				json stagesInfo = data["stages"]["list"];
+
+				std::unordered_set<std::size_t> res;
+
+				for (auto&& stage : stagesInfo) {
+
+					if (stage.contains("isScalable")) {
+
+						auto&& id = detail::nameToId(stages, stage["name"]);
+
+						if (id.has_value())
+							res.insert(id.value());
+						else
+							return {};
+					}
+				}
+
+				return res;
+			}
+		};
+	}
+}
