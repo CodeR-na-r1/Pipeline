3350fe579d45304770efbb72dc836bc255fd36d6
Vlad Sizyh <sizyh1375@gmail.com>
2024-04-18 23:17:55 +0800

    write sendler broker [--no stable]

---
+++ src/NetworkBroker/IBrokerSendler.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Pipeline {
+
+    namespace Broker {
+
+        template<typename BrokerOutputT>
+        class IBrokerSendler {
+
+        public:
+
+            virtual void connect() = 0;
+
+            virtual bool handleMessage(BrokerOutputT) = 0;
+        };
+    }
+}
